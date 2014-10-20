0b2cabe5d2406ef0588e3a54046a5920c99c6e13
ajhoekst <AJH>
2014-10-19 21:20:15 -0400

    Fixed SDL
    Upgraded to SDL 2.0.3

--- src/impl/notes.cpp
+++ src/impl/notes.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <cmath>
 
-//#include "SDL/SDL.h"
+#include "SDL2/SDL.h"
 
 using namespace std;
 
---
+++ src/incl/AMUS_audio_io.hpp
@@ -0,0 +1,4 @@
+#ifndef _AMUS_AUDIO_IO_H
+#define _AMUS_AUDIO_IO_H
+
+#endif
