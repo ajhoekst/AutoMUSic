49f5b86262600557fb9d10dc0174d5c48d97d99f
ajhoekst <AJH>
2014-10-20 21:26:50 -0400

    Example Code for SDL2 Mixer

--- src/impl/AMUS_audio_io.cpp
+++ src/impl/AMUS_audio_io.cpp
@@ -3,14 +3,45 @@
 #include <iostream>
 
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_audio.h"
+#include "SDL2/SDL_mixer.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_AUDIO);
-    cout << SDL_INIT_AUDIO << endl;
-    cout << &SDL_Init << endl;
-    return 0;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+	fprintf(stderr, "Unable to initialize SDL: %s\n", SDL_GetError());
+	return 1;
+    }
+     
+    int audio_rate = 22050;
+    Uint16 audio_format = AUDIO_S16SYS;
+    int audio_channels = 2;
+    int audio_buffers = 4096;
+     
+    if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) != 0) {
+        fprintf(stderr, "Unable to initialize audio: %s\n", Mix_GetError());
+        exit(1);
+    }
+    
+    Mix_Chunk *sound = NULL;
+ 
+    sound = Mix_LoadWAV("/Users/Andrew/Desktop/mambo_no_5-lou_bega.wav");
+    if(sound == NULL) {
+            fprintf(stderr, "Unable to load WAV file: %s\n", Mix_GetError());
+    }   
+    
+    int channel;
+ 
+    channel = Mix_PlayChannel(-1, sound, 0);
+    if(channel == -1) {
+            fprintf(stderr, "Unable to play WAV file: %s\n", Mix_GetError());
+    }
+    
+    while(Mix_Playing(channel) != 0);
+ 
+    Mix_FreeChunk(sound);
+     
+    Mix_CloseAudio();
+    SDL_Quit();
 }
